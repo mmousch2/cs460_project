2f760168ea67092faa4a511cc50b8d820bdb3863
kpchi <dysprosphor@gmail.com>
2017-05-02 04:09:30 -0500

    AES

--- SecureSMS/app/src/main/java/kpmm/securesms/AESEncryption.java
+++ SecureSMS/app/src/main/java/kpmm/securesms/AESEncryption.java
@@ -31,7 +31,7 @@ public class AESEncryption {
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.ENCRYPT_MODE, keyspec);
             encodedBytes = cipher.doFinal(text.getBytes());
-            return byte2hex(encodedBytes);
+            return toHex(encodedBytes);
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
         }
@@ -44,7 +44,7 @@ public class AESEncryption {
         try {
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.DECRYPT_MODE, keyspec);
-            decodedBytes = cipher.doFinal(hex2byte(text.getBytes()));
+            decodedBytes = cipher.doFinal(toByte(text));
             return new String(decodedBytes);
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
@@ -53,37 +53,69 @@ public class AESEncryption {
         return "fail ded";
     }
 
-    public static String byte2hex(byte[] b) {
+//    public static String byte2hex(byte[] b) {
+//
+//        String hs = "";
+//        String stmp = "";
+//
+//        for (int n = 0; n < b.length; n++) {
+//            stmp = Integer.toHexString(b[n] & 0xFF);
+//            if (stmp.length() == 1)
+//                hs += ("0" + stmp);
+//            else
+//                hs += stmp;
+//        }
+//
+//        return hs.toUpperCase();
+//    }
+//
+//    public static byte[] hex2byte(byte[] b) {
+//        if ((b.length % 2) != 0)
+//            throw new IllegalArgumentException("hello");
+//
+//        byte[] b2 = new byte[b.length / 2];
+//
+//        for (int n = 0; n < b.length; n += 2) {
+//            String item = new String(b, n, 2);
+//            b2[n / 2] = (byte) Integer.parseInt(item, 16);
+//        }
+//
+//        return b2;
+//    }
 
-        String hs = "";
-        String stmp = "";
-
-        for (int n = 0; n < b.length; n++) {
-            stmp = Integer.toHexString(b[n] & 0xFF);
-            if (stmp.length() == 1)
-                hs += ("0" + stmp);
-            else
-                hs += stmp;
-        }
-
-        return hs.toUpperCase();
+    public static String toHex(String txt) {
+        return toHex(txt.getBytes());
     }
 
-    public static byte[] hex2byte(byte[] b) {
-        if ((b.length % 2) != 0)
-            throw new IllegalArgumentException("hello");
+    public static String fromHex(String hex) {
+        return new String(toByte(hex));
+    }
 
-        byte[] b2 = new byte[b.length / 2];
+    public static byte[] toByte(String hexString) {
+        int len = hexString.length() / 2;
+        byte[] result = new byte[len];
+        for (int i = 0; i < len; i++)
+            result[i] = Integer.valueOf(hexString.substring(2 * i, 2 * i + 2),
+                    16).byteValue();
+        return result;
+    }
 
-        for (int n = 0; n < b.length; n += 2) {
-            String item = new String(b, n, 2);
-            b2[n / 2] = (byte) Integer.parseInt(item, 16);
+    public static String toHex(byte[] buf) {
+        if (buf == null)
+            return "";
+        StringBuffer result = new StringBuffer(2 * buf.length);
+        for (int i = 0; i < buf.length; i++) {
+            appendHex(result, buf[i]);
         }
-
-        return b2;
+        return result.toString();
     }
 
     public SecretKeySpec getSks() {
         return sks;
     }
+    private final static String HEX = "0123456789ABCDEF";
+    private static void appendHex(StringBuffer sb, byte b) {
+        sb.append(HEX.charAt((b >> 4) & 0x0f)).append(HEX.charAt(b & 0x0f));
+    }
+
 }
