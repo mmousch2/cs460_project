9c2f0b41d6028d0032008f5716a392cf0a15ce32
kpchi <dysprosphor@gmail.com>
2017-05-02 05:58:20 -0500

    KeyStore added, still broken and janky.
    KeyProperties and other KeyStore libraries still not found by Android Studio.

--- SecureSMS/app/src/main/java/kpmm/securesms/AESEncryption.java
+++ SecureSMS/app/src/main/java/kpmm/securesms/AESEncryption.java
@@ -2,10 +2,12 @@ package kpmm.securesms;
 
 import android.util.Base64;
 
+import java.security.KeyStore;
 import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -14,6 +16,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class AESEncryption {
     private SecretKeySpec sks = null;
+    private static final byte[] ivBytes = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
     public AESEncryption() {
         try {
@@ -21,7 +24,15 @@ public class AESEncryption {
             sr.setSeed("FOR THE LOVE OF SECURITY AND BUZZWORDS LET THIS WORK".getBytes());
             KeyGenerator kg = KeyGenerator.getInstance("AES");
             kg.init(256, sr);
-            sks = new SecretKeySpec((kg.generateKey()).getEncoded(), "AES");
+            SecretKey key = kg.generateKey();
+            sks = new SecretKeySpec(key.getEncoded(), "AES");
+
+            KeyStore ks = KeyStore.getInstance("AndroidKeyStore");
+            boolean success = ks.put("AESKeyCS460", key.getEncoded());
+            if (!success) {
+                int errorCode = ks.getLastError();
+                throw new RuntimeException("Keystore Error: " + errorCode);
+            }
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
         }
@@ -30,7 +41,7 @@ public class AESEncryption {
     public static String encryptKey(String text, SecretKeySpec keyspec) throws Exception {
         byte[] encodedBytes = null;
         try {
-            Cipher cipher = Cipher.getInstance("AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
             cipher.init(Cipher.ENCRYPT_MODE, keyspec);
             encodedBytes = cipher.doFinal(text.getBytes("UTF-8"));
             return Base64.encodeToString(encodedBytes, Base64.NO_WRAP);
@@ -44,7 +55,7 @@ public class AESEncryption {
     public static String decryptKey(String text, SecretKeySpec keyspec) throws Exception {
         byte[] decodedBytes = null;
         try {
-            Cipher cipher = Cipher.getInstance("AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
             cipher.init(Cipher.DECRYPT_MODE, keyspec);
             decodedBytes = cipher.doFinal(Base64.decode(text, Base64.NO_WRAP));
             return new String(decodedBytes, "UTF-8");
